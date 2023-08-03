21524abab0519049fe8ac5a59a8fd3523cc600a5
Evandro Viegas <evandrooviegas@gmail.com>
2023-08-03 20:35:02 +0100

    updated sanity studio

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-07-29'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-08-03'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
